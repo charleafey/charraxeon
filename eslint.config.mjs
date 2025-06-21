4ec0712514c748e9346fb0a440288296266fac12
chelffey <chaffeychelsea@gmail.com>
2025-06-21 21:07:14 +1000

    set up eslint and prettier formatting to work harmoniously

---
+++ eslint.config.mjs
@@ -0,0 +1,58 @@
+import { defineConfig } from 'eslint/config';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import react from 'eslint-plugin-react';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+});
+
+export default defineConfig([
+    {
+        extends: compat.extends(
+            'eslint:recommended',
+            'plugin:@typescript-eslint/recommended',
+            'plugin:react/recommended',
+            'plugin:prettier/recommended',
+        ),
+
+        plugins: {
+            '@typescript-eslint': typescriptEslint,
+            react,
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+
+            parser: tsParser,
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+        },
+
+        rules: {
+            'prettier/prettier': [
+                'error',
+                {
+                    trailingComma: 'all',
+                    tabWidth: 4,
+                    semi: true,
+                    singleQuote: true,
+                    bracketSpacing: true,
+                    eslintIntegration: true,
+                    printWidth: 120,
+                },
+            ],
+        },
+    },
+]);
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 export const HomePage: React.FC = () => {
-    return (<>
-         <h1>Charraxeon</h1>
-      <p>Hi there, I'm Charley! I create illutrations and merchandise with a warm and soft style. I'm inspired by how light forms colour and shapes around me, and do my best to imbue some of its magic into my artwork. I also love cats and nature!</p>
-    </>)
-}
+    return (
+        <>
+            <h1>Charraxeon</h1>
+            <p>
+                Hi there, I&apos;m Charley! I create illutrations and merchandise with a warm and soft style. I&apos;m
+                inspired by how light forms colour and shapes around me, and do my best to imbue some of its magic into
+                my artwork. I also love cats and nature!
+            </p>
+        </>
+    );
+};

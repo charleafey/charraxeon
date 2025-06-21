09d7c9bf88b2a69bde8e844e6b8fd184bca81b8e
chelffey <chaffeychelsea@gmail.com>
2025-06-21 20:19:32 +1000

    updated layout things

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const HomePage: React.FC = () => {
+    return (<>
+         <h1>Charraxeon</h1>
+      <p>Hi there, I'm Charley! I create illutrations and merchandise with a warm and soft style. I'm inspired by how light forms colour and shapes around me, and do my best to imbue some of its magic into my artwork. I also love cats and nature!</p>
+    </>)
+}
